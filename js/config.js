e29dd9e57ba6e29c25ad445d083c86cec9860f43
root <root@ip-10-11-2-143.ec2.internal>
2020-09-03 13:42:17 +0000

    basecommit

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_SnKwkly8Z',
+        userPoolClientId: '4iuscmlpqd3eeondsr5n2ksmhj',
+        region: 'us-east-1'
+    },
+    api: {
+        invokeUrl: 'https://ieyuhf9qxc.execute-api.us-east-1.amazonaws.com/prod/'
+    }
+};
---
+++ js/ride.js
@@ -0,0 +1,41 @@
+/*global WildRydes _config*/
+
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
+
+(function rideScopeWrapper($) {
+    var authToken;
+    WildRydes.authToken.then(function setAuthToken(token) {
+        if (token) {
+            authToken = token;
+        } else {
+            window.location.href = '/signin.html';
+        }
+    }).catch(function handleTokenError(error) {
+        alert(error);
+        window.location.href = '/signin.html';
+    });
+    function requestUnicorn(pickupLocation) {
+        $.ajax({
+            method: 'POST',
+            url: _config.api.invokeUrl + '/',
+            headers: {
+                Authorization: authToken
+            },
+            data: JSON.stringify({
+                PickupLocation: {
+                    Latitude: pickupLocation.latitude,
+                    Longitude: pickupLocation.longitude
+                }
+            }),
+            contentType: 'application/json',
+            success: completeRequest,
+            error: function ajaxError(jqXHR, textStatus, errorThrown) {
+                console.error('Error requesting ride: ', textStatus, ', Details: ', errorThrown);
+                console.error('Response: ', jqXHR.responseText);
+                alert('An error occured when requesting your unicorn:\n' + jqXHR.responseText);
+            }
+        });
+    }
+}(jQuery));
+
