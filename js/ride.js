12fe1002f85447c9348d9e77867235374c9d195b
root <root@ip-10-11-2-143.ec2.internal>
2020-09-04 12:10:46 +0000

    preReview

--- js/ride.js
+++ js/ride.js
@@ -15,7 +15,7 @@ UATMANAGEMENT.map = UATMANAGEMENT.map || {};
         alert(error);
         window.location.href = '/index.html';
     });
-    function requestStart() {
+    function requestStatus() {
         $.ajax({
             method: 'GET',
             url: _config.api.invokeUrl + '/',
@@ -41,8 +41,62 @@ UATMANAGEMENT.map = UATMANAGEMENT.map || {};
         $('#output').append(table);
         $('#table').append(trHTML);        
 }
+function requestStart() {
+    $.ajax({
+        method: 'GET',
+        url: _config.api.invokeUrl + '/',
+        headers: {
+            Authorization: authToken
+        },
+        cache: false,
+        contentType: 'application/json',
+        success: completeRequest,
+         error: function (jqXHR, textStatus, errorThrown) {
+            alert("error occurred while get data");
+        }
+    });
+}
+function completeRequest(result) {
+     var table = '';
+     var trHTML = '';
+     table += '<table class="table table-bordered table-striped" id=table><tr><th>ECS_ServiceName</th><th>Task_DesiredCount</th><th>Task_RunningCount</th></tr></table>'
+    $.each(result, function (i, item) {
+        trHTML += '<tr><td>' + item.ECS_ServiceName + '</td><td>' + item.Task_DesiredCount + '</td><td>' + item.Task_RunningCount + '</td></tr>';
+    });
+    $('#output').empty();
+    $('#output').append(table);
+    $('#table').append(trHTML);        
+}
+function requestStop() {
+    $.ajax({
+        method: 'GET',
+        url: _config.api.invokeUrl + '/',
+        headers: {
+            Authorization: authToken
+        },
+        cache: false,
+        contentType: 'application/json',
+        success: completeRequest,
+         error: function (jqXHR, textStatus, errorThrown) {
+            alert("error occurred while get data");
+        }
+    });
+}
+function completeRequest(result) {
+     var table = '';
+     var trHTML = '';
+     table += '<table class="table table-bordered table-striped" id=table><tr><th>ECS_ServiceName</th><th>Task_DesiredCount</th><th>Task_RunningCount</th></tr></table>'
+    $.each(result, function (i, item) {
+        trHTML += '<tr><td>' + item.ECS_ServiceName + '</td><td>' + item.Task_DesiredCount + '</td><td>' + item.Task_RunningCount + '</td></tr>';
+    });
+    $('#output').empty();
+    $('#output').append(table);
+    $('#table').append(trHTML);        
+}
     $(function onDocReady() {
-        $('#requestStatus').click(handleRequestClick);
+        $('#requestStatus').click(handleRequestStatusClick);
+        $('#startRequest').click(handleRequestStartClick);
+        $('#requestStop').click(handleRequestStopClick);
         UATMANAGEMENT.authToken.then(function updateAuthMessage(token) {
             if (token) {
                 displayUpdate('You are authenticated. Click to see your <a href="#authTokenModal" data-toggle="modal">auth token</a>.');
@@ -53,8 +107,14 @@ UATMANAGEMENT.map = UATMANAGEMENT.map || {};
             $('#noApiMessage').show();
         }
     });
-    function handleRequestClick() {
+    function handleRequestStatusClick() {
+        requestStatus();
+    }
+    function handleRequestStartClick() {
         requestStart();
+    }
+    function handleRequestStopClick() {
+        requestStop();
     }
     function displayUpdate(text) {
         $('#updates').append($('<h1 style="text-align:center;">UAT MANAGEMENT</h1>'));
@@ -63,4 +123,3 @@ UATMANAGEMENT.map = UATMANAGEMENT.map || {};
         $('#output').append($('<h1 style="text-align:left;">' + response + '</h1>'));
     }
 }(jQuery));
-
